f590c7966d25954af0723dff4a9122f15c7f949d
Nathan Chevalier <cerberusgfx@gmail.com>
2023-08-14 15:34:07 -0500

    Stores added to database, outputting list to DOM

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,12 @@
 import { flowersList } from "./flowers.js";
+import { storesList } from "./stores.js";
 
 const render = async () => {
   const flowersHTML = await flowersList();
+  const storesHTML = await storesList();
   const container = document.querySelector("#container");
   container.innerHTML = flowersHTML;
+  container.innerHTML += storesHTML;
 };
 
 render();
