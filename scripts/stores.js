bbf24d4a61dedefa435b151a8e289ccee27decca
Nathan Chevalier <cerberusgfx@gmail.com>
2023-08-15 15:10:37 -0500

    Matching store to nurseries

--- scripts/stores.js
+++ scripts/stores.js
@@ -8,13 +8,19 @@ const getStores = async () => {
   return stores;
 };
 
-// ? Link Table, Contains both Nursery & Distributor information
 const getDistributors = async () => {
+  const response = await fetch("http://localhost:8088/distributors");
+  const distributors = await response.json();
+  return distributors;
+};
+
+// ? Link Table, Contains both Nursery & Distributor information
+const getDistributorNursery = async () => {
   const response = await fetch(
     "http://localhost:8088/distributorNursery?_expand=distributor&_expand=nursery"
   );
-  const distributors = await response.json();
-  return distributors;
+  const distributorNursery = await response.json();
+  return distributorNursery;
 };
 
 const getNurseries = async () => {
@@ -33,14 +39,32 @@ const getNurseryFlowers = async () => {
 export const storesList = async () => {
   const stores = await getStores();
   const distributors = await getDistributors();
+  const distributorNursery = await getDistributorNursery();
   const nurseries = await getNurseries();
   const nurseryFlowers = await getNurseryFlowers();
   const flowers = await getFlowers();
   let html = `<h1> Our Stores </h1>
                   <section class="stores__list">`;
+
+  // ? Matches store to nurseries Store -> Distributor -> Nursery
   const storesHTML = stores.map((store) => {
+    const storeDistributor = distributors.find(
+      (fDistributor) => fDistributor.id === store.distributorId
+    );
+    const distributorNurseries = distributorNursery.filter(
+      (dNurseries) => dNurseries.distributorId === storeDistributor.id
+    );
+    const nurseriesArray = distributorNurseries.map((nurseryOut) => {
+      return nurseries.find((nursery) => nurseryOut.nurseryId === nursery.id);
+    });
+    const nurseriesOutput = nurseriesArray.map((nArray) => {
+      return `<li> ${nArray.name} from ${nArray.city}, ${nArray.state} </li>`;
+    });
+
     return `<div class="stores__single">${store.name} - ${store.city}, ${store.state} - 
             Receiving flowers from ${store.distributor.name} in ${store.distributor.city}, ${store.distributor.state}</div>
+            <h2>${store.name}'s Nurseries</h2>
+            <ul>${nurseriesOutput}</ul>
             `;
   });
   html += storesHTML.join("");
