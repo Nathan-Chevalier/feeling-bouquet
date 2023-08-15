71e0ad6b0ae5aec7e4a1cd613640dd2f13779e46
Nathan Chevalier <cerberusgfx@gmail.com>
2023-08-15 14:07:49 -0500

    Importing all required data to stores.js

--- scripts/stores.js
+++ scripts/stores.js
@@ -1,4 +1,6 @@
-export const getStores = async () => {
+import { getFlowers } from "./flowers.js";
+
+const getStores = async () => {
   const response = await fetch(
     "http://localhost:8088/stores?_expand=distributor"
   );
@@ -6,8 +8,34 @@ export const getStores = async () => {
   return stores;
 };
 
+// ? Link Table, Contains both Nursery & Distributor information
+const getDistributors = async () => {
+  const response = await fetch(
+    "http://localhost:8088/distributorNursery?_expand=distributor&_expand=nursery"
+  );
+  const distributors = await response.json();
+  return distributors;
+};
+
+const getNurseries = async () => {
+  const response = await fetch("http://localhost:8088/nurseries");
+  const nurseries = await response.json();
+  return nurseries;
+};
+
+// ? Link table
+const getNurseryFlowers = async () => {
+  const response = await fetch("http://localhost:8088/nurseryFlowers");
+  const nurseryFlowers = await response.json();
+  return nurseryFlowers;
+};
+
 export const storesList = async () => {
   const stores = await getStores();
+  const distributors = await getDistributors();
+  const nurseries = await getNurseries();
+  const nurseryFlowers = await getNurseryFlowers();
+  const flowers = await getFlowers();
   let html = `<h1> Our Stores </h1>
                   <section class="stores__list">`;
   const storesHTML = stores.map((store) => {
